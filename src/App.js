a605ef433cbb9d1a7fa6ed2c61b6517a09a7dc20
Will Ting <theweicode@gmail.com>
2019-06-07 15:38:35 -0700

    complete first lesson

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="parent">
+   <div className="child"></div>
     </div>
   );
 }
 
 export default App;
+
+
+/* https://cssreference.io/property/transition-timing-function/
+https://www.w3schools.com/css/css_pseudo_classes.asp */
