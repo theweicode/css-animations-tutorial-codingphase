f2a5bce6293419480cbe30437fc872cc2c8806f5
Will Ting <theweicode@gmail.com>
2019-06-10 13:20:03 -0700

    button animated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.scss';
 function App() {
   return (
     <div className="parent">
-   <div className="child"></div>
+   <div className="child">
+
+     <button className="btn">Button</button>
+   </div>
     </div>
   );
 }
@@ -13,4 +16,8 @@ export default App;
 
 
 /* https://cssreference.io/property/transition-timing-function/
-https://www.w3schools.com/css/css_pseudo_classes.asp */
+https://www.w3schools.com/css/css_pseudo_classes.asp 
+
+summary: make sure to understand if we want to change child values on hover then we set it on the hover in the parent. 
+but if we only want to have the change happen when hover over child then declare the :hover on the child class instead
+*/
